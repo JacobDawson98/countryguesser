8993608bd96f541b8942dcf2ba0554e38dc8e91f
JacobDawson98 <jacobpauldawson@gmail.com>
2021-06-16 19:02:33 -0500

    Try specifying multiple aliases for root cf distribution

--- terraform/www/route53.tf
+++ terraform/www/route53.tf
@@ -15,14 +15,14 @@ resource "aws_route53_record" "root-a" {
   }
 }
 
-resource "aws_route53_record" "www-a" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
+# resource "aws_route53_record" "www-a" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
+#     zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
