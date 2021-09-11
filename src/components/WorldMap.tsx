fc3c504c33ac9231d986f752dc7eac216589d008
Jacob Dawson <jacobpauldawson@gmail.com>
2021-09-11 11:56:56 -0500

    feat: Click and drag world map, select countries

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,9 +1,29 @@
-import WorldMapSvg from '../assets/world.svg';
+import WorldGeography from '../assets/world-110m.json';
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
 
 function WorldMap() {
   return (
-    <img src={WorldMapSvg} alt="Map of the World" />
-  );
+    <div>
+      <ComposableMap>
+        <ZoomableGroup zoom={1}>
+          <Geographies geography={WorldGeography}>
+            {({ geographies }) =>
+              geographies.map((geography) => { 
+                console.log(geography);
+                return(
+                <Geography key={geography.rsmKey} geography={geography} 
+                fill="#E4E5E6"
+                stroke="#232323"
+                strokeWidth="0.3"
+                className="focus:outline-none"
+                />
+              )})
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
+  )
 }
 
 export default WorldMap;
