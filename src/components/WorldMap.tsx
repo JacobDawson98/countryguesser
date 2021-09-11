7aa91af8b5615722af2678c870935c0f176b5abc
Jacob Dawson <jacobpauldawson@gmail.com>
2021-09-11 12:12:34 -0500

    refactor: Add id to world map

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,29 +1,34 @@
-import WorldGeography from '../assets/world-110m.json';
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
+import WorldGeography from "../assets/world-110m.json";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 
 function WorldMap() {
   return (
-    <div>
+    <div id="worldMap">
       <ComposableMap>
         <ZoomableGroup zoom={1}>
           <Geographies geography={WorldGeography}>
             {({ geographies }) =>
-              geographies.map((geography) => { 
-                console.log(geography);
-                return(
-                <Geography key={geography.rsmKey} geography={geography} 
-                fill="#E4E5E6"
-                stroke="#232323"
-                strokeWidth="0.3"
-                className="focus:outline-none"
+              geographies.map((geography) => (
+                <Geography
+                  key={geography.rsmKey}
+                  geography={geography}
+                  fill="#E4E5E6"
+                  stroke="#232323"
+                  strokeWidth="0.3"
+                  className="focus:outline-none"
                 />
-              )})
+              ))
             }
           </Geographies>
         </ZoomableGroup>
       </ComposableMap>
     </div>
-  )
+  );
 }
 
 export default WorldMap;
