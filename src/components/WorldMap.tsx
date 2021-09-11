d0ca4dc878221a93b4f5226d99d19c3d576abb0a
Jacob Dawson <jacobpauldawson@gmail.com>
2021-09-11 12:15:03 -0500

    Add world map click and drag navigation (#4)

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,8 +1,33 @@
-import WorldMapSvg from '../assets/world.svg';
+import WorldGeography from "../assets/world-110m.json";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 
 function WorldMap() {
   return (
-    <img src={WorldMapSvg} alt="Map of the World" />
+    <div id="worldMap">
+      <ComposableMap>
+        <ZoomableGroup zoom={1}>
+          <Geographies geography={WorldGeography}>
+            {({ geographies }) =>
+              geographies.map((geography) => (
+                <Geography
+                  key={geography.rsmKey}
+                  geography={geography}
+                  fill="#E4E5E6"
+                  stroke="#232323"
+                  strokeWidth="0.3"
+                  className="focus:outline-none"
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
   );
 }
 
--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
   it('should render the world map', async () => {
-    render(<App />);
-    const mapOfTheWorld = await screen.findByAltText('Map of the World');
-    expect(mapOfTheWorld).toBeInTheDocument();
+    const rendered = render(<App />);
+    expect(rendered.container.querySelector('#worldMap')).toBeDefined();
   });
 });
