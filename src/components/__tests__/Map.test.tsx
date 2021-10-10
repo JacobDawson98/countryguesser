95f468210d8db7757ba6064d1dd4e8e8037d42ef
Jacob Dawson <jacobpauldawson@gmail.com>
2021-10-10 10:14:54 -0500

    fix: Provide all props to map in test

--- src/components/__tests__/Map.test.tsx
+++ src/components/__tests__/Map.test.tsx
@@ -2,9 +2,15 @@ import { render } from "@testing-library/react";
 import { mapsToGeography } from "../Game";
 import Map from "../Map";
 
-describe("WorldMap", () => {
+describe("Map", () => {
   it("should render the world map when given the world map", () => {
-    const rendered = render(<Map geography={mapsToGeography["WorldMap"]} />);
+    const rendered = render(
+      <Map
+        geography={mapsToGeography["WorldMap"]}
+        selectedCountry=""
+        setSelectedCountryRsmKey={jest.fn()}
+      />
+    );
     const worldMap = rendered.getByTestId("Map");
 
     expect(worldMap).toBeDefined();
