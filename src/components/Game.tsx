f6c9acd068fb8464329ae6aae957bc2c9078d036
Jacob Dawson <jacobpauldawson@gmail.com>
2021-10-10 10:09:34 -0500

    feat: Add dialog to Game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import WorldGeography from "../assets/world-110m.json";
 import Map from "./Map";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContentText from "@mui/material/DialogContentText";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import Box from "@mui/system/Box";
 
 enum Maps {
   WorldMap = "WorldMap",
@@ -14,12 +25,61 @@ export const mapsToGeography: Record<
 };
 
 function Game() {
-  const [mapSelection] = useState<Maps>(Maps.WorldMap);
-  // const [countryToFind, setCountryToFind] = useState("");
-  const [selectedCountry, setSelectedCountry] = useState("");
+  const [mapSelection, setMapSelection] = useState<Maps>(Maps.WorldMap);
+  const [selectedCountryRsmKey, setSelectedCountryRsmKey] = useState("");
+  const [isPlayingGame, setIsPlayingGame] = useState(false);
+  function startGame(): () => void {
+    return () => setIsPlayingGame(true);
+  }
+  function onSelectMap(event: SelectChangeEvent<Maps>): () => void {
+    // @ts-expect-error autofill of arbitrary value is not handled
+    return () => setMapSelection(event.target.value);
+  }
+
   return (
     <div>
-      <Map geography={mapsToGeography[mapSelection]} selectedCountry={selectedCountry} setSelectedCountry={(rsmKey: string) => setSelectedCountry(rsmKey)} />
+      <Dialog maxWidth="lg" open={!isPlayingGame} onClose={startGame()} disableEscapeKeyDown>
+        <DialogTitle>Countryguesser</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Select the map you want to play</DialogContentText>
+          <Box
+            noValidate
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              m: "auto",
+              width: "fit-content",
+            }}
+          >
+            <FormControl sx={{ mt: 4 }}>
+              <InputLabel htmlFor="map">Map</InputLabel>
+              <Select
+                autoFocus
+                value={mapSelection}
+                onChange={(event) => onSelectMap(event)}
+                label="Map"
+                inputProps={{
+                  name: "map",
+                  id: "map",
+                }}
+              >
+                <MenuItem value={Maps.WorldMap}>World Map</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={startGame()}>Start Game</Button>
+        </DialogActions>
+      </Dialog>
+      <Map
+        geography={mapsToGeography[mapSelection]}
+        selectedCountry={selectedCountryRsmKey}
+        setSelectedCountryRsmKey={(rsmKey: string) =>
+          setSelectedCountryRsmKey(rsmKey)
+        }
+      />
     </div>
   );
 }
