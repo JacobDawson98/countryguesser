4d29c59cb4f8cf1480d3b1f9cde97757753025be
Jacob Dawson <jacobpauldawson@gmail.com>
2021-10-10 21:05:43 -0500

    fix: remove unnecessary await

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
-  it('should render the world map', async () => {
+  it('should render the world map', () => {
     const rendered = render(<App />);
     expect(rendered.container.querySelector('#worldMap')).toBeDefined();
   });
